60381eb8926a91bee9103ca9d40160724aad57d4
tiagopbop <tiagopbop@gmail.com>
2023-10-20 21:55:13 +0100

    Termninal Opens

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
-
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TerminalSize terminalSize = new TerminalSize(200, 200);
+        DefaultTerminalFactory terminalFactory = new
+                DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
