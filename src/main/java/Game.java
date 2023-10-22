019ed3862f742f0f5343cc5ef284fdee1e54dc8d
tiagopbop <tiagopbop@gmail.com>
2023-10-22 13:04:40 +0100

    7 in progress

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,6 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +10,8 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game(){
 
@@ -29,17 +32,22 @@ public class Game {
 
 
     }
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+    }
 
     private void draw() throws IOException
     {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() throws IOException {
         try {
             draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
